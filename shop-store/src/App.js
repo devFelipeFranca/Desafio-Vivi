1ee09037578543bc4b0d95638caa7c30d3117429
Felipe França <felipefrancafj@gmail.com>
2021-03-11 06:38:59 -0300

    Configurando Rotas

--- shop-store/src/App.js
+++ shop-store/src/App.js
@@ -1,10 +1,28 @@
+import { Provider } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
+import store from './store';
+
+import './css/bulma.min.css'
 
 function App() {
-  return (
-    <div >
-      App
-    </div>
+  return (  
+    <Provider store={ store }>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/CC">
+            <Cart />
+          </Route>
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
